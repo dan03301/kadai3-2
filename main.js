abd432198f9169a3ada50a01cc465e54ede2bd3e
高島檀 <takashimamadan@takashimadannoMacBook-Pro.local>
2020-05-31 13:56:27 +0900

    再提出

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
-const add = document.getElementById('add');
-const task = document.getElementById('task');
-const tableDataId = document.getElementById('tableData');
-const table = document.getElementById('table');
+const add = document.getElementById("add");
+const task = document.getElementById("task");
+const tableDataId = document.getElementById("tableData");
+const table = document.getElementById("table");
 
 const todos = [];
 
-add.addEventListener('click', () => {
-  if (!task.value == '') {
-    const todo = { task: `${task.value}`, status: '作業中' };
+add.addEventListener("click", () => {
+  if (!task.value == "") {
+    const todo = { task: task.value, status: "作業中" };
     todos.push(todo); // 追加
     showTodos();
     console.log(todos);
   } else {
-    alert('タスクを入力してください。');
+    alert("タスクを入力してください。");
   }
 });
 
@@ -22,42 +22,49 @@ const showTodos = () => {
   }
 
   todos.forEach((todo, index) => {
-    const createId = document.createElement('td');
-    const createComment = document.createElement('td');
+    const createId = document.createElement("td");
+    const createComment = document.createElement("td");
 
-    createId.textContent = `${index}`;
-    createComment.textContent = `${todo.task}`;
+    createId.textContent = index;
+    createComment.textContent = todo.task ;
 
-    const createTr = document.createElement('tr');
-    
+    const createTr = document.createElement("tr");
+    const createStatus = document.createElement("td");
+
+    const statusFunc = createStatusButtonFunc(todo.status);
+    const removeFunc = createRemoveButtonFunc(index);
+
+
+    createStatus.appendChild(statusFunc);
+    createStatus.appendChild(removeFunc);
     createTr.appendChild(createId);
     createTr.appendChild(createComment);
-    createTr.appendChild(createButtons(todo.status,index));
+    createTr.appendChild(createStatus);
+
     table.appendChild(createTr);
 
-    const deleteButton = document.getElementById(`${index}`);
-    deleteButton.addEventListener('click', () => {
+    const deleteButton = document.getElementById(index);
+    deleteButton.addEventListener("click", () => {
       todos.splice(deleteButton, 1);
       showTodos();
     });
   });
-  
-  task.value = '';
+
+  task.value = "";
 };
 
-function createButtons(status,index) {
-  const createStatus = document.createElement('td');
-  const createStatusButton = document.createElement('button');
-  const createRemoveButton = document.createElement('button');
+function createStatusButtonFunc(status) {
+  const createStatusButton = document.createElement("button");
+  createStatusButton.textContent = status;
+  createStatusButton.setAttribute("class", "statusButton");
 
-  createStatusButton.textContent = `${status}`;
-  createStatusButton.setAttribute('class', 'statusButton');
-  createRemoveButton.textContent = '削除';
-  createRemoveButton.setAttribute('id', `${index}`);
-
-  createStatus.appendChild(createStatusButton);
-  createStatus.appendChild(createRemoveButton);
-
-  return createStatus;
+  return createStatusButton;
 }
 
+function createRemoveButtonFunc(index) {
+  const createRemoveButton = document.createElement("button");
+  createRemoveButton.textContent = "削除";
+  createRemoveButton.setAttribute("id", index);
+
+  return createRemoveButton;
+}
